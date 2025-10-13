7697bc5d14d63604f7be96c0ab303d4e89e19a58
Maady321 <sabarikrishnan994@gmail.com>
2025-10-14 01:14:39 +0530

    location trasing with phone number using  python code

--- loop.py
+++ loop.py
@@ -1,5 +1,18 @@
+import phonenumbers
+from phonenumbers import geocoder
+import time
+import random
 
-i = 1
-while i <= 5:
-    print(i)
-    i += 1
+def start_phone_tracer(target):
+    print(f"[+] PhoneTracer v2.1 - OSINT")
+    print(f"[*] Target: {target}")
+    print(f"[*] Initiating trace ...")
+
+    p = phonenumbers.parse(target, None)
+    r = geocoder.description_for_number(p, "en")
+
+    print(f"[+] Location: {r}")
+    print(f"[+] Trace complete")
+
+# Call the function
+start_phone_tracer("+91 9048682360")
